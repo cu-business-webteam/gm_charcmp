67c3b4e93e8cea72c48b5ec24d5877b7526dbeb7
uniblab <uniblab@hotmail.com>
2017-07-19 16:00:56 -0400

    initial seed

---
+++ AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "CharCmp" )]
+[assembly: AssemblyDescription( "Compares two files character-by-character using specified code page or windows-1252 if omitted." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Johnson at Cornell University" )]
+[assembly: AssemblyProduct( "CharCmp" )]
+[assembly: AssemblyCopyright( "Copyright © Johnson at Cornell University 2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "8c052fc8-cceb-4f1a-a75f-9532798937fd" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Linq;
+
+namespace CharCmp {
+
+	public static class Program {
+
+		[System.STAThread]
+		public static System.Int32 Main( System.String[] args ) {
+			if ( ( null == args ) || ( args.Length < 4 ) || ( 0 != ( args.Length % 2 ) ) ) {
+				PrintUsage();
+				return 2;
+			}
+
+			System.String left = null;
+			System.String right = null;
+			System.String cp = null;
+			var l = args.Length;
+			var i = 0;
+			do {
+				if ( args[ i ].Equals( "/fileA", System.StringComparison.OrdinalIgnoreCase ) ) {
+					left = args[ ++i ].Trim();
+				} else if ( args[ i ].Equals( "/fileB", System.StringComparison.OrdinalIgnoreCase ) ) {
+					right = args[ ++i ].Trim();
+				} else if ( args[ i ].Equals( "/cp", System.StringComparison.OrdinalIgnoreCase ) ) {
+					cp = args[ ++i ].Trim();
+				} else {
+					PrintUsage();
+					return 2;
+				}
+			} while ( ++i < l );
+			if ( System.String.IsNullOrEmpty( left ) || System.String.IsNullOrEmpty( right ) ) {
+				PrintUsage();
+				return 2;
+			}
+
+			if ( System.String.IsNullOrEmpty( cp ) ) {
+				cp = "windows-1252";
+			}
+			System.Text.Encoding encoding = System.Text.Encoding.GetEncoding( cp );
+			if ( null == encoding ) {
+				System.Int32 cpn;
+				if ( System.Int32.TryParse( cp, out cpn ) ) {
+					encoding = System.Text.Encoding.GetEncoding( cpn );
+				}
+			}
+			if ( null == encoding ) {
+				encoding = System.Text.Encoding.GetEncoding( "windows-1252" );
+			}
+
+			return IsSameByCharValue( left, right, encoding )
+				? 0
+				: 1
+			;
+		}
+
+		private static System.Boolean IsSameByCharValue( System.String left, System.String right, System.Text.Encoding encoding ) {
+#if TRACE || DEBUG
+			if ( null == right ) {
+				throw new System.ArgumentNullException( "right" );
+			} else if ( null == left ) {
+				throw new System.ArgumentNullException( "left" );
+			} else if ( null == encoding ) {
+				throw new System.ArgumentNullException( "encoding" );
+			}
+#endif
+			using ( var leftF = System.IO.File.Open( left, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read ) ) {
+				using ( var rightF = System.IO.File.Open( right, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read ) ) {
+					return IsSameByCharValue( leftF, rightF, encoding );
+				}
+			}
+		}
+
+		private static System.Boolean IsSameByCharValue( System.IO.Stream left, System.IO.Stream right, System.Text.Encoding encoding ) {
+#if TRACE || DEBUG
+			if ( null == right ) {
+				throw new System.ArgumentNullException( "right" );
+			} else if ( null == left ) {
+				throw new System.ArgumentNullException( "left" );
+			} else if ( null == encoding ) {
+				throw new System.ArgumentNullException( "encoding" );
+			}
+#endif
+			using ( var leftR = new System.IO.StreamReader( left, encoding, true, 16384, true ) ) {
+				using ( var rightR = new System.IO.StreamReader( right, encoding, true, 16384, true ) ) {
+					return IsSameByCharValue( leftR, rightR );
+				}
+			}
+		}
+		private static System.Boolean IsSameByCharValue( System.IO.StreamReader left, System.IO.StreamReader right ) {
+#if TRACE || DEBUG
+			if ( null == right ) {
+				throw new System.ArgumentNullException( "right" );
+			} else if ( null == left ) {
+				throw new System.ArgumentNullException( "left" );
+			}
+#endif
+			var output = true;
+			System.String l;
+			System.String r;
+			System.Boolean reading = true;
+			do {
+				l = left.ReadLine();
+				r = right.ReadLine();
+				if ( !IsSameByCharValue( l, r ) ) {
+					output = false;
+					reading = false;
+				} else if ( ( null == l ) || ( null == r ) ) {
+					reading = false;
+				}
+			} while ( reading );
+			return output;
+		}
+		private static System.Boolean IsSameByCharValue( System.String left, System.String right ) {
+			return ( ( null == left ) && ( null == right ) )
+				? true
+				: ( ( null == left ) != ( null == right ) )
+					? false
+					: System.String.Equals( left.Normalize(), right.Normalize(), System.StringComparison.Ordinal )
+			;
+		}
+
+
+		private static void PrintUsage() {
+			var err = System.Console.Error;
+			err.WriteLine( "No! No! No! Use it like this, Einstein:" );
+			err.WriteLine( "CharCmp.exe /fileA pathNameOfFileA /fileB pathNameOfFileB [/cp codePageNameOrNumber]" );
+			err.WriteLine( "Example: CharCmp.exe /fileA .\\groceries.txt /fileB \\users\\home\\foo123\\groc.list.txt" );
+			err.WriteLine( "Example: CharCmp.exe /fileA .\\letters\\to\\cleo.txt /fileB ..\\something.else /cp us-ascii" );
+			err.WriteLine( "The default code-page is Windows-1252" );
+		}
+
+	}
+
+}
